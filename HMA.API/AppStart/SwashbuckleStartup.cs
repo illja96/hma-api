2b96d35e81b3518075d4658a02329b48454fc8b0
Illia Khovanskyi <saskiuhia@gmail.com>
2020-03-19 19:07:15 +0200

    Add Swashbuckle

---
+++ HMA.API/AppStart/SwashbuckleStartup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace HMA.API.AppStart
+{
+    internal static class SwashbuckleStartup
+    {
+        public static void Init(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(swaggerUi =>
+            {
+                swaggerUi.RoutePrefix = string.Empty;
+
+                var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                swaggerUi.SwaggerEndpoint(
+                    $"/swagger/{assemblyVersion}/swagger.json",
+                    "HMA API");
+            });
+        }
+
+        public static void Init(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<OpenApiInfo>(configuration.GetSection(nameof(OpenApiInfo)));
+
+            services.AddSwaggerGen(swaggerGen =>
+            {
+                var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+                var openApiInfo = configuration.GetSection(nameof(OpenApiInfo)).Get<OpenApiInfo>();
+                openApiInfo.Version = assemblyVersion;
+
+                swaggerGen.SwaggerDoc(assemblyVersion, openApiInfo);
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                swaggerGen.IncludeXmlComments(xmlPath);
+            });
+        }
+    }
+}
--- HMA.API/Startup.cs
+++ HMA.API/Startup.cs
@@ -1,3 +1,4 @@
+using HMA.API.AppStart;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -15,15 +16,17 @@ namespace HMA.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            SwashbuckleStartup.Init(services, Configuration);
+
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            SwashbuckleStartup.Init(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
